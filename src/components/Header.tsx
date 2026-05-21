787f70f734e541bf89a7d50ae297261a49714dce
milan-nqode <milan.masnikosa@nqode.io>
2026-05-21 14:33:33 +0200

    fix: change description to h2 tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         className="size-48 rounded-full shadow-md hover:shadow-2xl transition-shadow duration-300 ease-in-out"
       />
       <h1 className="font-bold text-4xl transition-colors duration-500">Milan Masnikosa</h1>
-      <h3>Full Stack Developer @ <a className="hover:underline" href="https://nqode.io" target="_blank">nqode.io</a></h3>
+      <h2 className="text-base">Full Stack Developer @ <a className="hover:underline" href="https://nqode.io" target="_blank">nqode.io</a></h2>
     </div>
   );
 }
