f4a35049efddf1b85462e140126f33fb3d5e9bff
milan-nqode <milan.masnikosa@nqode.io>
2026-05-21 14:31:04 +0200

    fix: convert image to webp, added meta description

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@ export default function Header() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center">
       <img
-        src="/milan.jpeg"
+        src="/milan.webp"
         alt="Milan Masnikosa"
         className="size-48 rounded-full shadow-md hover:shadow-2xl transition-shadow duration-300 ease-in-out"
       />
       <h1 className="font-bold text-4xl transition-colors duration-500">Milan Masnikosa</h1>
+      <h3>Full Stack Developer @ <a className="hover:underline" href="https://nqode.io" target="_blank">nqode.io</a></h3>
     </div>
   );
 }
